37559d3cd63f81d6a0a0c4f647c9cf40d338def0
Danish khan <danishshoukat1993@gmail.com>
2026-08-19 00:51:40 +0500

    income and expense  complete

--- src/modules/category/pages/Categories.tsx
+++ src/modules/category/pages/Categories.tsx
@@ -5,9 +5,15 @@ import { buttonList } from "../../../constant/categoryConstant";
 import { categoryList, type CategoryType } from "../../../constant/dummy";
 import CategoryHighlightsCard from "../components/CategoryHighlightsCard";
 import CateogoryCard from "../components/CateogoryCard";
+import { useState } from "react";
 
 const Categories = () => {
   const navigate = useNavigate();
+
+  const [categoryType, setCategoryType] = useState<"expense" | "income">(
+    "expense",
+  );
+  console.log(categoryType);
 
   return (
     <div className="text-white">
@@ -19,7 +25,10 @@ const Categories = () => {
           </CustomText>
         </div>
         <div>
-          <CustomSwitch listButton={buttonList} />
+          <CustomSwitch
+            listButton={buttonList}
+            setCategoryType={setCategoryType}
+          />
         </div>
       </div>
       <div>
@@ -47,17 +56,29 @@ const Categories = () => {
           </div>
         </div>
         <div className="my-2 grid grid-cols-3 gap-3">
-          {categoryList?.map((category: CategoryType) => {
-            return (
-              <CateogoryCard
-                key={category?.id}
-                color={category?.color}
-                categoryName={category?.categoryName}
-                iconName={category?.iconName}
-                spending={40}
-              />
-            );
-          })}
+          {categoryType === "expense"
+            ? categoryList?.map((category: CategoryType) => {
+                return (
+                  <CateogoryCard
+                    key={category?.id}
+                    color={category?.color}
+                    categoryName={category?.categoryName}
+                    iconName={category?.iconName}
+                    spending={40}
+                  />
+                );
+              })
+            : categoryList?.map((category: CategoryType) => {
+                return (
+                  <CateogoryCard
+                    key={category?.id}
+                    color={category?.color}
+                    categoryName={category?.categoryName}
+                    iconName={category?.iconName}
+                    spending={40}
+                  />
+                );
+              })}
         </div>
       </div>
     </div>
